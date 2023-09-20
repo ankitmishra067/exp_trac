aa3496fb74c35fbf16e5d9328997ec98403f3dd8
Ankit Kumar Mishra <ankitkumar@jmangroup.com>
2023-09-20 16:59:54 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,13 @@ const cors = require('cors')
 require('dotenv').config();
 const app = express()
 const { db } = require('./db/db')
+const {readdirSync} = require('fs')
 const PORT = process.env.PORT
 
 app.use(cors())
 app.use(express.json())
+
+readdirSync('./routes').map((route) => app.use('/api/v1' ,require('./routes/' + route)))
 
 const server =()=>{
     db()
